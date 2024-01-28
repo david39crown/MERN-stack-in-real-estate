d1b62834c152044b8ac1675be282da897567d301
david39crown <david39crown@gmail.com>
2024-01-27 17:09:38 -0800

    create signin in api route

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 export const signup=async(req,res,next)=>
 {
@@ -17,6 +18,25 @@ export const signup=async(req,res,next)=>
             {
                 next(error)
             }
+}
+
+export const signin=async(req,res,next)=>
+{
+    const {email,password}=req.body
+    try
+    {
+        const validUser=await User.findOne({email})
+        if(!validUser) return next(errorHandler(404,'user not found'))
+        const validPassword=bcryptjs.compareSync(password,validUser.password) //puriyala
+        if (!validPassword) return next(errorHandler(401,"Invalid password"))
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const{password :pass,...rest} =validUser._doc
+    res.cookie('access token',token,{httpOnly:true}).status(200).json(rest)
+    } 
+    catch (error)
+     {
+        next(error)
+     }
 }
 
 //npm i bcryptjs --for  password hash
