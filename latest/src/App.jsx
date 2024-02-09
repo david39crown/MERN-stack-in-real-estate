9dd0fc98c3e144e419b42691b955d0374d5eb12a
david39crown <david39crown@gmail.com>
2024-02-09 06:41:07 -0800

    update the header and make the profile page is private

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './assets/pages/SignUp'
 import Profile from './assets/pages/Profile'
 import About from './assets/pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/privateRoute'
 export default function App() {
   return (
     <BrowserRouter>
@@ -14,8 +15,10 @@ export default function App() {
       <Route path="/" element={<Home />}/>
       <Route path="/sign-in" element={<SignIn />}/>
       <Route path="/sign-up" element={<SignUp />}/>
-      <Route path="/profile" element={<Profile />}/>
       <Route path="/about" element={<About />}/>
+      <Route  element={<PrivateRoute/>}>
+      <Route path="/profile" element={<Profile />}/>
+      </Route>
   </Routes> 
   </BrowserRouter> 
   
--- latest/src/components/Header.jsx
+++ latest/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 export default function Header() {
+
+ const {currentUser}=useSelector(state=>state.user)
   return (
     <header className='bg-emerald-500 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
@@ -18,7 +21,16 @@ export default function Header() {
             <ul  className='flex gap-4'>
                  <Link to='/'>   <li className='hidden sm:inline text-neutral-950 font-bold hover:text-white'>Home</li></Link>
                  <Link to='about'>  <li className='hidden sm:inline text-neutral-950 font-bold  hover:text-white'>About</li></Link>
-                 <Link to='sign-in'>  <li className='text-neutral-950 font-bold hover:text-white'>Sign In</li></Link>
+                
+                 <Link to='profile'> 
+                 {currentUser ? 
+                 (
+                    <img className='rounded-full h-7 w-7 object-cover'src={currentUser.avatar} alt="profile" />
+                 ):
+                  <li className='text-neutral-950 font-bold hover:text-white'>
+                    Sign In</li>
+                 }
+                  </Link>
                 </ul>
         </div>
     </header>
