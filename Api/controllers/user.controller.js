d3bea57ab0efc1da2afdbe07f57b8f6f235ad754
david39crown <david39crown@gmail.com>
2024-02-22 06:56:30 -0800

    creating delete user api route

--- Api/controllers/user.controller.js
+++ Api/controllers/user.controller.js
@@ -36,3 +36,14 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser=async (req,res,next)=> {
+    if(req.user.id !==req.params.id) return next(errorHandler(401,"you can delete  only your own account"));
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("user as been deleted")
+    } catch (error) {
+      next(error)
+    }
+
+}
