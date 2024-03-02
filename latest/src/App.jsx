745492f15702eaf0a8a645d7df039829307ab6ff
david39crown <david39crown@gmail.com>
2024-03-02 11:53:03 -0800

    add upload listing images functionality

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -26,3 +26,64 @@ export default function App() {
   
   )
 }
+
+
+
+/* 
+return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="*" element={<Home />}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/about" element={<About />}/>
+      <Route  element={<PrivateRoute/>}>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/create listing" element={<CreateListing />}/>
+      </Route>
+  </Routes> 
+  </BrowserRouter> 
+  
+  )
+}
+ */
+
+
+
+
+
+
+
+
+
+/* 
+ return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ExploreWithoutNavbar />} />
+        <Route element={<Header />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function ExploreWithoutNavbar() {
+  return (
+    <Routes>
+      <Route path="/" element={<Explore />} />
+    </Routes>
+  )
+}
+
+
+*/
