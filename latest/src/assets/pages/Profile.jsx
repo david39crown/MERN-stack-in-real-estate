055bf02078083e73d87152e988593306ddd1045a
david39crown <david39crown@gmail.com>
2024-03-07 10:05:06 -0800

    adding delete user listing functionality

--- latest/src/assets/pages/Profile.jsx
+++ latest/src/assets/pages/Profile.jsx
@@ -167,6 +167,26 @@ export default function Profile() {
     }
     
   }
+
+
+  const handleListingDelete=async(listingId)=>
+  {
+    try {
+      const res=await fetch(`/Api/listing/delete/${listingId}`,{
+        method:"DELETE",
+      })
+      const data=await res.json()
+      if(data.sucsess===false)
+      {
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev)=>prev.filter((listing)=>listing._id!==listingId))
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
 
@@ -259,7 +279,10 @@ export default function Profile() {
                  <p>{listing.name}</p>
                 </Link>
                 <div className="flex flex-col item-center">
-                  <button className='text-red-700 uppercase'>Delete</button>
+                  <button
+                     onClick={()=>handleListingDelete(listing._id)} 
+                     className='text-red-700 uppercase'
+                  >Delete</button>
                   <button className='text-green-700 uppercase'>Edit</button>
                 </div>
 
