1e52364571d1aab1481a744136453a6830935699
david39crown <david39crown@gmail.com>
2024-03-07 23:52:47 -0800

    complete image slider in listing page is completed

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/privateRoute'
 import CreateListing from './assets/pages/CreateListing'
 import UpdateListing from './assets/pages/UpdateListing'
+import Listing from './assets/pages/Listing'
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,9 +19,10 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />}/>
       <Route path="/sign-up" element={<SignUp />}/>
       <Route path="/about" element={<About />}/>
+      <Route path="/listing/:listingId" element={<Listing />}/>
       <Route  element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />}/>
-         <Route path="/create-listing" element={<CreateListing />}/>
+         <Route path="/create listing" element={<CreateListing />}/>
          <Route path="/update-listing/:listingId" element={<UpdateListing />}/>
       </Route>
   </Routes> 
